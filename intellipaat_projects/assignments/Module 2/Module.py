538b593951b6a17e2635e477cbabb5d3ce4e3643
Samudra Roy <91413821+samudra-roy@users.noreply.github.com>
2024-07-09 23:04:05 +0530

    Add files via upload

---
+++ intellipaat_projects/assignments/Module 2/Module.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def addition(a,b):
+    return a+b
+def subtraction(a,b):
+    return a-b
+def multiplication(a,b):
+    return a*b
+def division(a,b):
+    return a/b
+
